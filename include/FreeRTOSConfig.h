14b9c2cd2924113ab9abe8457ae0ff25cdb977d0
João Vitor S. Anacleto <22249746+digitalphoton@users.noreply.github.com>
2025-02-25 18:06:37 -0300

    Modificar FreeRTOSConfig.h para os requisitos do projeto

--- include/FreeRTOSConfig.h
+++ include/FreeRTOSConfig.h
@@ -20,9 +20,9 @@
 #define configIDLE_SHOULD_YIELD                 1
 #define configUSE_TASK_NOTIFICATIONS            1
 #define configTASK_NOTIFICATION_ARRAY_ENTRIES   3
-#define configUSE_MUTEXES                       0
-#define configUSE_RECURSIVE_MUTEXES             0
-#define configUSE_COUNTING_SEMAPHORES           0
+#define configUSE_MUTEXES                       1
+#define configUSE_RECURSIVE_MUTEXES             1
+#define configUSE_COUNTING_SEMAPHORES           1
 #define configUSE_ALTERNATIVE_API               0 /* Deprecated! */
 #define configQUEUE_REGISTRY_SIZE               10
 #define configUSE_QUEUE_SETS                    0
@@ -54,7 +54,7 @@
 
 /* Co-routine related definitions. */
 #define configUSE_CO_ROUTINES                   0
-#define configMAX_CO_ROUTINE_PRIORITIES         1
+#define configMAX_CO_ROUTINE_PRIORITIES         2
 
 /* Software timer related definitions. */
 #define configUSE_TIMERS                        1
