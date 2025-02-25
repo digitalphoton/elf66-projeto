2ccb6f9805cf55f4ecae4b07a3b3ee38d10c1aa9
João Vitor S. Anacleto <22249746+digitalphoton@users.noreply.github.com>
2025-02-25 18:07:23 -0300

    Testar dois processos com preempção

--- src/main.c
+++ src/main.c
@@ -7,20 +7,20 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-const char *compile_dt = __DATE__ " " __TIME__;
-
 void vTaskHello(void *pvParameters);
+void vTaskWorld(void *pvParameters);
 
 int main(void) {
-    /* Initialize board components. */
+    // Initialize clock
     SystemCoreClockUpdate();
 
     /* Initialize UART for libc function calls. */
     USART_Printf_Init(115200);
 
-    printf("FreeRTOS demo compiled AT %s\r\n", compile_dt);
+    printf("\n#------------------------------------------------#\n\n");
 
     xTaskCreate(vTaskHello, "HELLO", 256, NULL, 4, NULL);
+    xTaskCreate(vTaskWorld, "WORLD", 256, NULL, 4, NULL);
 
     vTaskStartScheduler();
 
@@ -31,7 +31,15 @@ int main(void) {
 
 void vTaskHello(void *pvParameters) {
     for (;;) {
-        printf("Hello world? @%lu\r\n", xTaskGetTickCount());
+        printf("Hello\n");
         vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
+void vTaskWorld(void *pvParameters)
+{
+    for (;;)
+    {
+        printf("World\n");
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
